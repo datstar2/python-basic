18a605e528daca0f2bc5fdb14261cf25529a8108
datstar2 <pssd12@gmail.com>
2020-08-26 16:08:04 +0900

    들여쓰기연습

--- 01/hello.py
+++ 01/hello.py
@@ -1,2 +1,26 @@
-# python-basics
-## 00. 설치하기
+#helloworld.py
+
+
+print('Hello World')
+
+
+a = 1
+if a < 2:
+    print(a)
+    print('big')
+
+b = 20
+
+
+def my_func(i):
+    if i >10:
+        print('big')
+    else:
+        print('small')
+
+
+# 함수 호출
+my_func(20)
+
+
+
